9e81e563a4e9f8e256bf173b49494203cb3d5b64
Philipp Schmitt <philipp@schmitt.co>
2017-07-21 08:26:54 +0200

    Simplify check-config script

--- check-config.sh
+++ check-config.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-[[ -z $(docker exec -it hass \
-    python -m homeassistant -c /config --script check_config | \
-    grep -v "Testing configuration at" | tee /dev/stderr) ]]
+docker exec -it hass \
+    python -m homeassistant -c /config --script check_config
