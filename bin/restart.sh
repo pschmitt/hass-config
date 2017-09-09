9927a3662dbd5b6a1fd35d724ff4fbbef35922c9
Philipp Schmitt <philipp@schmitt.co>
2017-09-09 21:45:05 +0200

    Add a force option to force restart hass despite a (currently) invalid config

--- bin/restart.sh
+++ bin/restart.sh
@@ -2,7 +2,15 @@
 
 cd "$(dirname $(realpath $0))"
 
+hass_restart() {
+    docker-compose -f ../docker-compose.yml restart hass
+}
+
 HASS_SERVICE=home-assistant
+
+case "$1" in
+    -f|--force|force) FORCE=1 ;;
+esac
 
 if ! systemctl is-active "$HASS_SERVICE" > /dev/null
 then
@@ -10,10 +18,13 @@ then
     sudo systemctl daemon-reload
     sudo systemctl start "$HASS_SERVICE"
     echo 'Done!'
+elif [[ -n "$FORCE" ]]
+then
+    hass_restart
 elif ./check-config.sh > /dev/null
 then
     echo "Config checks out ✓"
-    docker-compose -f ../docker-compose.yml restart hass
+    hass_restart
 else
     echo "Config is invalid. Please fix it." >&2
     exit 2
