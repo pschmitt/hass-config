40806e75fa9c52359c853f086e32dbc682a82eff
Philipp Schmitt <philipp@schmitt.co>
2018-01-08 10:22:18 +0100

    Linting

--- bin/travis_gen_secrets.py
+++ bin/travis_gen_secrets.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 import os
 import re
 import yaml
 
-basedir = os.path.realpath(os.path.dirname(__file__))
+BASEDIR = os.path.realpath(os.path.dirname(__file__))
 
-HASS_SECRETS_FILE = os.path.join(basedir, '../config/hass/secrets.yaml')
+HASS_SECRETS_FILE = os.path.join(BASEDIR, '../config/hass/secrets.yaml')
 TRAVIS_SECRETS_FILE = os.path.join(
-    basedir, '../config/hass/.travis/secrets.yaml')
+    BASEDIR, '../config/hass/.travis/secrets.yaml')
 
 TRAVIS_FLOAT = 0.0
 TRAVIS_INT = 0
@@ -16,15 +17,15 @@ TRAVIS_STR = 'travis_secret_16'
 TRAVIS_FILE = './config/hass/.travis/file'
 
 with open(HASS_SECRETS_FILE, 'r') as stream:
-    hass_config = yaml.load(stream)
+    HASS_CONFIG = yaml.load(stream)
 
-travis_config = {}
+TRAVIS_CONFIG = {}
 
-mac_addr_re = re.compile(r'([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})')
-url_re = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|'
-                     '(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+MAC_ADDR_RE = re.compile(r'([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})')
+URL_RE = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
+                    r'[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
-for k, v in hass_config.items():
+for k, v in HASS_CONFIG.items():
     t = type(v)
     if t == float:
         travis_val = TRAVIS_FLOAT
@@ -33,15 +34,15 @@ for k, v in hass_config.items():
     elif t == str:
         if v.startswith('/'):
             travis_val = TRAVIS_FILE
-        elif re.match(mac_addr_re, v):
+        elif re.match(MAC_ADDR_RE, v):
             travis_val = '00:01:02:03:04:05'
-        elif re.match(url_re, v):
+        elif re.match(URL_RE, v):
             travis_val = 'http://localhost:8080/index.html'
         else:
             travis_val = TRAVIS_STR
-    travis_config[k] = travis_val
+    TRAVIS_CONFIG[k] = travis_val
 
 with open(TRAVIS_SECRETS_FILE, 'w') as outfile:
-    yaml.dump(travis_config, outfile, default_flow_style=False)
+    yaml.dump(TRAVIS_CONFIG, outfile, default_flow_style=False)
     print('Wrote travis secret file to {}'.format(
         os.path.realpath(outfile.name)))
